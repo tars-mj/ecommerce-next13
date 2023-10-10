20efb7eb5b68365328c79bd86cdc327df882b065
Maciej Judka <maciej.judka@gmail.com>
2023-10-10 12:41:22 +0200

    add: test page 3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 export default function Home() {
 	return (
-		<main className="flex min-h-screen flex-col items-center justify-between p-24">test2</main>
+		<main className="flex min-h-screen flex-col items-center justify-between p-24">test3</main>
 	);
 }
