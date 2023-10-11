baf7fad056f57d034ec35b48c0bfc59dd1b87145
Maciej Judka <maciej.judka@gmail.com>
2023-10-11 13:31:21 +0200

    fix: ngrok address4

--- src/app/products/[page]/page.tsx
+++ src/app/products/[page]/page.tsx
@@ -3,6 +3,15 @@ import { ProductList } from "@/components/ProductList";
 
 export async function generateStaticParams() {
 	const products = await getProducts(undefined, 30);
+
+	console.log("Wynik z getProducts:", products);
+
+	if (!products) {
+		throw new Error("Nie udało się pobrać produktów");
+	}
+
+	console.log("Wynik z getProducts:", products);
+
 	const numOfPages = Math.ceil(products.length / 12);
 	const pages = Array.from({ length: numOfPages }, (_, i) => i + 1);
 	return pages.map((page) => ({ params: { page: page.toString() } }));
